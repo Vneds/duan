0adedaea7d3027c363a638d8de32afa913331f1f
John Doe <johndoe@gmail.com>
2023-03-17 13:37:00 +0700

    them san pham

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?php 
-    $host = 'localhost:3307';
+    $host = 'localhost';
     $dbName = 'duan1';
     $userName = 'root';
-    $password = '';
+    $password = '123456';
     try {
         // Kết nối
         $conn = new PDO("mysql:host=$host;dbname=$dbName", $userName, $password);
