02a20dad5739ef143cc6c3ca9ddfda994c777562
John Doe <johndoe@gmail.com>
2023-04-09 20:05:12 +0700

    sua lai doi mk

--- model/connect_db.php
+++ model/connect_db.php
@@ -1,9 +1,9 @@
 
 <?php
-    $host= 'localhost:3307';
+    $host= 'localhost';
     $dbName = 'duan1';
     $userName = 'root';
-    $password = '';
+    $password = '123456';
 
     try {
         // Kết nối
